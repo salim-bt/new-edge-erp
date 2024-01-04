024db10a69938348b6bb08ac3861687cbde3c6c0
salim pradhan <salimpradhan77291188@outlook.com>
2024-01-04 10:33:01 +0600

    added notification path

---
+++ apps/hrm/src/app/notifications/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+export default function Notifications(){
+
+    return <div className="flex flex-col w-full min-h-full items-center justify-center">
+        <h1 className="text-2xl font-bold">Notifications</h1>
+    </div>
+}
